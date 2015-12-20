c95a54f03a8821eed19dc25b325663085676e02b
jehna <jesse.luoto@gmail.com>
2015-12-20 22:43:27 +0200

    Fixed: User-specific refresh script

--- refresh.sh
+++ refresh.sh
@@ -1,1 +1,1 @@
-find ~/Library/Thunderbird/Profiles/uhoze3oz.default/ImapMail/by2prd0611.outlook.com/INBOX.sbd -iregex ".*[^\.]..." -exec grep "^Date: ...," {} \; | sed -e 's/Date: //g' > data.txt
+find ~/Library/Thunderbird/Profiles/ -name "INBOX" -exec grep "^Date: ...," {} \; | sed -e 's/Date: //g' > data.txt
